1680049da716d7630703d0c1d2e825810d7a3f42
7tuv <7tuv@example.com>
2019-02-01 02:00:37 +0900

    Add functions of addition and substraction

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,10 @@ module Lib
     ( genAssemblyCode
     ) where
 
+import Data.Char (isDigit)
+
+data Symbol = Number String | Plus | Minus deriving (Show)
+
 genAssemblyCode :: String -> [String]
 genAssemblyCode text =
     concat [assemblyCodeHead, assemblyCodeBody text]
@@ -16,7 +20,34 @@ assemblyCodeHead =
 assemblyCodeBody :: String -> [String]
 assemblyCodeBody text =
     [
-     "main:",
-     "  mov rax, " ++ text,
+     "main:"
+    ]
+    ++
+    (calcCode $ parse text)
+    ++
+    [
      "  ret"
     ]
+
+calcCode :: [Symbol] -> [String]
+calcCode [] = []
+calcCode (Number x : xs)         = ("  mov rax, " ++ x) : calcCode xs
+calcCode (Plus  : Number x : xs) = ("  add rax, " ++ x) : calcCode xs
+calcCode (Minus : Number x : xs) = ("  sub rax, " ++ x) : calcCode xs
+calcCode x = error "calcCode function failed"
+
+parse :: String -> [Symbol]
+parse ""        = []
+parse (x:xs)
+    | x == '+'  = Plus  : parse xs
+    | x == '-'  = Minus : parse xs
+    | isDigit x =
+        let len = numLength (x:xs)
+        in Number (take len $ x:xs) : (parse . drop len $ x:xs)
+    | otherwise   = error "parse function failed"
+
+numLength :: String -> Int
+numLength "" = 0
+numLength (x:xs)
+    | isDigit x = 1 + numLength xs
+    | otherwise = 0
